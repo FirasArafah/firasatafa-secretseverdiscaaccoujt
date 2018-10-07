aae6d71749edba53057919cdde98cf32de4d5be7
FirasArafah <43936321+FirasArafah@users.noreply.github.com>
2018-10-07 22:11:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,42 @@ client.on('message', msg => {
     msg.reply('Pong!');
   }
 });
+var PrEfix = "f";
+
+client.on('message', message => {
+
+  if (!message.content.startsWith(PrEfix)) return;
+
+  var args = message.content.split(' ').slice(1);
+
+  var argresult = args.join(' ');
+
+  if (message.author.id !== '478291914106339332') return;
+
+if (message.content.startsWith(PrEfix + 'wt')) {
+
+client.user.setActivity(argresult, {type:'WATCHING'});
+
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+
+} else 
+
+if (message.content.startsWith(PrEfix + 'ls')) {
+
+client.user.setActivity(argresult, {type:'LISTENING'});
+
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+
+} else 
+
+if (message.content.startsWith(PrEfix + 'st')) {
+
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+
+}
+
+});
 
 client.login(process.env.BOT_TOKEN);  //لا تحط التوكن حقك هنا
