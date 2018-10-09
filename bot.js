8749fe34c3be2792f49807c678c1ae7a7441b32f
FirasArafah <43936321+FirasArafah@users.noreply.github.com>
2018-10-09 20:47:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame("Nathaan , Nicolas.","https://www.twitch.tv/idk");
+  client.user.setGame("Nathaan.","https://www.twitch.tv/idk");
 });
 
 client.on('message', msg => {
