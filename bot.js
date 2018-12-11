b074965fc5b580726ac2228506987e3bea6a9c83
FirasArafah <43936321+FirasArafah@users.noreply.github.com>
2018-12-11 19:01:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame("Off , #aLeRhAbiYh...","https://www.twitch.tv/idk");
+  client.user.setGame("Off ","https://www.twitch.tv/idk");
 });
 
 client.on('message', msg => {
