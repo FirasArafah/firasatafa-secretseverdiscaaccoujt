2f6291a5c642771312743becd8c944292cfc26f2
FirasArafah <43936321+FirasArafah@users.noreply.github.com>
2019-01-31 21:03:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,20 +53,6 @@ if (message.content.startsWith(PrEfix + 'st')) {
 });
 
 
-client.on('message', msg => {
-
-    if (msg.content == '$join') {
-        if (msg.member.voiceChannel) {
-
-     if (msg.member.voiceChannel.joinable) {
-         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
-     }
-    }
-}
-})
-client.on('ready', () => {
-    client.channels.get("538066541179043851").join(); 
-    });
 
 
 
