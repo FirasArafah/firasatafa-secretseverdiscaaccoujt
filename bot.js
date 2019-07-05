ae622a2cae154af4a17142bfabf082f03d0cfccb
FirasArafah <43936321+FirasArafah@users.noreply.github.com>
2019-07-06 00:36:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame("Nothing To Say..","https://www.twitch.tv/idk");
+  client.user.setGame("#Melitary Group .","https://www.twitch.tv/idk");
 });
 
 var PrEfix = "f";
